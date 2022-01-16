d5b4e3889840fad1b66a5383c686842eb7de29a0
priyanshijain432 <74758485+priyanshijain432@users.noreply.github.com>
2022-01-16 13:52:55 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,15 @@
+class atm:
+    print("Insert Your ATM Card")
+    def_init_(pinNumber):
+        pinNumber=input("enter pin number")
+
+def WithDraw_money(self,amount,pinNumber):
+    amount=str(input("enter amount"))
+    print("amount+withDrawed")
+
+def Balance_Enquiry(self,accountNumber):
+    accountNumber=int(input("enter your account number"))
+    print("accountNumber+has+amount+balance")
+
+WithDraw_money()
+Balance_Enquiry()
